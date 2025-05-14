d0740da5cea0f045c4de68090f888ae34a696bdf
subvertdissent <subvertdissent@gmail.com>
2025-05-13 21:34:42 -0500

    Update stats and main

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-from stats import get_num_words
+import sys
+from sys import argv
+from stats import report, get_num_words, generated_dict, get_char_count, get_book_text 
 
-get_num_words()
+if len(argv) < 2:
+    sys.exit("Usage: python3 main.py <path_to_book>")
+
+book_path = argv[1]
+
+def main(a):
+    if len(book_path) < 1: 
+        sys.exit("huh")    
+    else:
+        report(book_path)
+
+main(book_path)
+
--- stats.py
+++ stats.py
@@ -1,19 +1,59 @@
+from sys import argv
+
+# get_book_text takes a file argument and parses the text.
+
 def get_book_text(book):
     with open(book) as f:
         file_contents = f.read()
     return file_contents
 
-def get_num_words():
+# get_num_words counts total words in book
+
+def get_num_words(a):
     num_words = 0 
-    book_string = get_book_text("/home/dennisb/Projects/gopherus/subvertdissent/bookbot/books/frankenstein.txt")
-    book_text = book_string.split()
+    book_path = a
+    read_book = get_book_text(a)
+    book_text = read_book.split()
     for i in range(0, len(book_text)):
         num_words += 1
-    print(f"{num_words} words found in the document")
+    return num_words   
 
-def get_char_count():
-    chars  = {
-            "a": 0, "b": 0, "c": 0, "d": 0, "e": 0, "f": 0, "g": 0, "h": 0, "i": 0,
-            "j": 0, "k": 0, "l": 0, "m": 0, "n": 0, "o": 0, "p": 0, "q": 0, "r": 0,
-            "s": 0, "t": 0, "u": 0, "v": 0, "w": 0, "x": 0, "y": 0, "z": 0
-            }
+# generated_dict generates a dictionary
+
+def generated_dict(a):
+    chars = {}
+    read_book = get_book_text(a)
+    book_path = read_book.lower().split()
+    for char in book_path:
+        for letter in char:
+            if letter != chars:
+                chars[letter] = 0
+    return chars 
+
+def get_char_count(a):
+    chars = generated_dict(a)
+    book_path = get_book_text(a).lower().split()
+    for char in book_path:
+        for letter in char:
+            if letter in chars:
+                chars[letter] += 1
+    return chars
+
+def sort_on(dict):
+    return dict["num"]
+
+def report(a):
+    book = a
+    char_report = get_char_count(a)
+    sorted_chars = sorted(char_report.items(), key=lambda x:x[1], reverse=True)
+    char_dict = dict(sorted_chars)
+    word_count = get_num_words(a)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {a}")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for i in char_dict:
+        if i.isalpha():
+            print(f"{i}: {char_dict[i]}")
+    print("============= END ===============")
